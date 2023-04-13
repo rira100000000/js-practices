4ab5adc56a623b916481557b4c3e322fb87feb27
rira <101010hayakawa@gmail.com>
2023-04-13 12:26:59 +0900

    厳密等価演算子を使用するように修正
    投下演算子を使用していた部分を厳密等価演算子を使用するように修正しました。

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,9 +1,9 @@
 for (let count = 1; count <= 20; count++) {
-  if (count % 15 == 0) {
+  if (count % 15 === 0) {
     console.log("FizzBuzz");
-  } else if (count % 5 == 0) {
+  } else if (count % 5 === 0) {
     console.log("Buzz");
-  } else if (count % 3 == 0) {
+  } else if (count % 3 === 0) {
     console.log("Fizz");
   } else {
     console.log(count);
